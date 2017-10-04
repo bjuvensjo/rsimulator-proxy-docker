f3a59daae56df3f89b4ac4cae2ca6c58744c932e
Magnus Bjuvensjö <magnus.bjuvensjo@evry.com>
2017-10-05 00:02:05 +0200

    Refactored to multi-stage build

--- entry.sh
+++ entry.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 mkdir $RSIMULATOR_HOME/configuration
-mv -n $JETTY_HOME/resources/* $RSIMULATOR_HOME/configuration/
-rm -f $JETTY_HOME/resources/*
-ln -s $RSIMULATOR_HOME/configuration/URIMapper.txt $JETTY_HOME/resources/URIMapper.txt 
-ln -s $RSIMULATOR_HOME/configuration/logback.groovy $JETTY_HOME/resources/logback.groovy 
-ln -s $RSIMULATOR_HOME/configuration/recorder.properties $JETTY_HOME/resources/recorder.properties 
+mv -n $JETTY_BASE/resources/* $RSIMULATOR_HOME/configuration/
+rm -f $JETTY_BASE/resources/*
+ln -s $RSIMULATOR_HOME/configuration/URIMapper.txt $JETTY_BASE/resources/URIMapper.txt 
+ln -s $RSIMULATOR_HOME/configuration/logback.groovy $JETTY_BASE/resources/logback.groovy 
+ln -s $RSIMULATOR_HOME/configuration/recorder.properties $JETTY_BASE/resources/recorder.properties 
 
-cd $JETTY_HOME; java -DrootPath=$RSIMULATOR_HOME -DuseRootRelativePath=true -jar start.jar
+cd $JETTY_BASE; java -jar $JETTY_HOME/start.jar
